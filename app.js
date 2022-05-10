85604adddfe8c0ca4444250521d4c3a54103d6d8
Victor <ajom9@hotmail.com>
2022-05-10 14:34:07 -0500

    Uso de listas normales y con objetos

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ new Vue ({
     return{
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-      chancePercent: 10
+      chancePercent: 10,
+      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+      pricesWithDays: [
+        {day:'Lunes' , value: 8400},
+        {day:'Martes' , value: 7900},
+        {day:'Miércoles' , value: 8200},
+        {day:'Jueves' , value: 9000},
+        {day:'Viernes' , value: 9400},
+        {day:'Sábado' , value: 10000},
+        {day:'Domingo' , value: 10200},
+      ]
     }
   }
 })
