2889b59a6252c39aa9aef92e56bb36befb64cfc1
Victor <ajom9@hotmail.com>
2022-05-10 14:53:11 -0500

    Se agregó el funcionamiento del v-on con click, mouseover y mouseout

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ new Vue ({
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       chancePercent: 10,
-      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+      //prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
       pricesWithDays: [
         {day:'Lunes' , value: 8400},
         {day:'Martes' , value: 7900},
@@ -15,7 +15,15 @@ new Vue ({
         {day:'Viernes' , value: 9400},
         {day:'Sábado' , value: 10000},
         {day:'Domingo' , value: 10200},
-      ]
+      ],
+      showPrices: false
+    }
+  },
+
+  methods: {
+    toggleShowPrices(){
+      this.showPrices = !this.showPrices
     }
   }
+
 })
